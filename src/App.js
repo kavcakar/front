4f4c53eb3093b997857aeb1bd291175feeef902e
Emre kavcakar <kavcakaremre@gmail.com>
2024-06-15 01:44:20 -0400

    install

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Movies from "./pages/Movies";
+import Genres from "./pages/Genres";
 
 const App = () => {
-  return <div>App</div>;
+  return;
+  <Router>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/movies" element={<Movies />} />
+      <Route path="/genres" element={<Genres />} />
+    </Routes>
+  </Router>;
 };
 
 export default App;
